70d0ce1806de06f9b72d7cfd995ba43193355f2a
Khudaat E. Mammadli <khudaatem@code.local>
2024-10-26 15:16:19 +0400

    added task

---
+++ task/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace TASK
+{
+    internal class Program
+    {
+
+        //    static void Main(string[] args)
+        //    #region task1
+        //    {
+        //        Console.WriteLine("Bir eded daxil edin: ");
+        //        int number = Convert.ToInt32(Console.ReadLine());
+
+        //        Console.WriteLine(" Void metodunun neticesi: ");
+        //        Print(number);
+
+        //        int result = Calculate(number);
+        //        {
+        //            Console.WriteLine($"Return metodunun neticesi; {result}");
+        //        }
+        //    }
+
+        //    static int Calculate(int n)
+        //    {
+        //        int sum = 0;
+        //        for (int i = 0; i <= n; i++)
+        //        {
+        //            if (i % 2 == 0)
+        //            {
+        //                sum += i;
+        //            }
+        //        }
+        //        return sum;
+        //    }
+
+        //    static void Print(int n)
+        //    {
+        //        int sum = 0;
+        //        for (int i = 0; i <= n; i++)
+        //        {
+        //            if (i % 2 == 0)
+
+        //            {
+        //                sum += i;
+        //            }
+
+        //            Console.WriteLine(sum);
+        //        }
+        //    }
+        //    #endregion
+        //    #region task2
+        //    {
+        //        string name = " Code Academy  ";
+
+        //    Console.WriteLine($"Original metn:\"{name}\"");
+        //        string result = Remove(name);
+        //    Console.WriteLine($"Bosluqlar silinmis metn:\"{result}\"");
+        //    }
+        //static string Remove(string name)
+        //{
+        //    return name;
+        //}
+        //#endregion
+    }
+
+}
+
+
+
